af98458301740449dbd4fce1b046abfb1f91484c
Waite Wang <waite@waite.wang>
2024-01-25 22:30:42 +0800

    feat: 自定义底部导航栏 tabBar

--- pages/index/index.js
+++ pages/index/index.js
@@ -91,7 +91,13 @@ Page({
 	 * 生命周期函数--监听页面显示
 	 */
 	onShow: function () {
-		
+		if (typeof this.getTabBar === 'function') {
+			this.getTabBar((tabBar) => {
+				tabBar.setData({
+					select: 0
+				})
+			})
+		}	
 	},
 
 	/**
