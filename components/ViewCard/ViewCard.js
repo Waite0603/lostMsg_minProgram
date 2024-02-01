db5fb56966d7c6bf67514e8d7b2a9c907ef807c6
Waite Wang <waite@waite.wang>
2024-02-02 00:13:24 +0800

    feat: 物品详情页布局实现

--- components/ViewCard/ViewCard.js
+++ components/ViewCard/ViewCard.js
@@ -1,24 +1,30 @@
 // components/ViewCard/ViewCard.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
 		listImage: Array
 	},
 
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
 
 	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		toDetail(e) {
+			let data = e.currentTarget.dataset;
+			wx.navigateTo({
+				url: '../../pages/infoDetail/infoDetail?data=' + JSON.stringify(data),
+			})
+		}
+	}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,56 +8,42 @@ Page({
 		listImage: [
 			{
 				name: 'AI动画',
-				price: '1w+/部',
-				title: '反邪教创意公益广告',
+				price: '2024-01-08',
+				title: '灯塔',
 				url: '/images/water_pic/000ac34008b0ba4c.jpg',
 				avatar: '/images/water_pic/000ac34008b0ba4c.jpg',
 			},
 			{
 				name: '宣传片',
-				price: '5w+/部',
-				title: '以数字化护航清远农业发展',
+				price: '2024-01-08',
+				title: '捡到一个城堡',
 				url: '/images/water_pic/000b42cae15622e0.jpg',
 				avatar: '/images/water_pic/000b42cae15622e0.jpg',
 			},
 			{
-				name: '科普类',
-				price: '5w+/部',
-				title: '在清远有多少健身房计划圈钱跑路？',
-				url: '/images/water_pic/000ac95750ac7399.jpg',
-				avatar: '/images/water_pic/000ac95750ac7399.jpg',
-			},
-			{
-				name: '比赛PPT',
-				price: '200/页',
-				title: '大数据赋能检察办案提升法律监督质效',
+				name: '123',
+				price: '2024-01-08',
+				title: '丢失两只狗狗',
 				url: '/images/water_pic/000acf666d991c39.jpg',
 				avatar: '/images/water_pic/000acf666d991c39.jpg',
 			},
 			{
 				name: '访谈',
-				price: '3w+/部',
-				title: '面对面访谈-如何快速实现清远农产品数字化',
+				price: '2024-01-08',
+				title: '捡到手机两部',
 				url: '/images/water_pic/000ad20b5e452b24.jpg',
 				avatar: '/images/water_pic/000ad20b5e452b24.jpg',
 			},
 			{
-				name: '解说类',
-				price: '1k/h',
-				title: '清城区VS清新区篮球赛',
-				url: '/images/water_pic/000ad3d42653f5f6.jpg',
-				avatar: '/images/water_pic/000ad3d42653f5f6.jpg',
-			},
-			{
 				name: '场景建模类',
-				price: '10w/部',
-				title: '某某石化加工厂模型工作原理',
+				price: '2024-01-08',
+				title: '某某石化加工厂模型',
 				url: '/images/water_pic/000ad6c520be9ec5.jpg',
 				avatar: '/images/water_pic/000ad6c520be9ec5.jpg',
 			},
 			{
-				name: '校园拍摄/街拍/婚拍',
-				price: '3千/天',
+				name: '校园拍摄',
+				price: '2024-01-08',
 				title: '情侣/闺蜜/对象写真艺术照',
 				url: '/images/water_pic/000ad6fa67b5ad96.jpg',
 				avatar: '/images/water_pic/000ad6fa67b5ad96.jpg',
@@ -67,6 +53,10 @@ Page({
 
 	changeTab(value) {
 		console.log(value.detail);
+	},
+
+	toDetail(e) {
+		console.log(e);
 	},
 
 	/**
